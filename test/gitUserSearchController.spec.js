1470c5d05cd7b5b5fd5119e1fc291be70a3d29d3
guidovitafinzi <guido.vitafinzi@gmail.com>
2015-03-19 11:10:29 +0000

    Added github api

--- test/gitUserSearchController.spec.js
+++ test/gitUserSearchController.spec.js
@@ -18,22 +18,34 @@ describe('GitUserSearchController', function() {
 
   describe('when searching for a user', function(){
 
+    var httpBackend;
+    beforeEach(inject(function($httpBackend) {
+      httpBackend = $httpBackend
+      httpBackend
+        .when("GET", "https://api.github.com/search/users?q=hello")
+        .respond(
+          { items: items }
+          );
+    }));
+
     var items = [
       {
         "login": "tansaku",
         "avatar_url": "https://avatars.githubusercontent.com/u/30216?v=3",
         "html_url": "https://github.com/tansaku"
-      },
+      }, 
       {
         "login": "stephenlloyd",
         "avatar_url": "https://avatars.githubusercontent.com/u/196474?v=3",
-        "html_url": "https://github.com/stephenlloyd" 
+        "html_url": "https://github.com/stephenlloyd"
       }
     ];
 
     it("displays search results", function() {
       scope.searchTerm = 'hello';
       scope.doSearch();
+      scope.$apply();
+      httpBackend.flush();
       expect(scope.searchResult.items).toEqual(items);
     });
 
